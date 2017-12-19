f2dddd68b28e919f77c091b344d2f30009b13d17
Seong ho Hong <grohong76@gmail.com>
2017-12-19 16:55:53 +0900

    set dom

--- src/js/app.js
+++ src/js/app.js
@@ -1,11 +1,26 @@
 console.log('my-note app.js');
 
 //DOM
-var storage = window.localStorage;
-var memo = document.getElementById('memo');
-var btn_savenote = document.getElementById('btn-savenote');
-var btn_savenote_file = document.getElementById('btn-savenote-file');
-var btn_newnote = document.getElementById('btn-newnote');
-var btn_fullscreen = document.getElementById('btn-fullscreen');
-var btn_about = document.getElementById('btn-about');
-var about = document.getElementById('about');
+$(function () {
+  console.log('start page');
+})
+
+$('.btn-newnote').click(function(){
+  console.log('click new note');
+});
+
+$('.btn-download').click(function() {
+  console.log('click download');
+})
+
+$('.btn-savenote').click(function() {
+  console.log('click save note');
+})
+
+$('.btn-fullscreen').click(function() {
+  console.log('click fullscreen note');
+})
+
+$('#about').click(function() {
+  console.log('click about wee-note');
+})
