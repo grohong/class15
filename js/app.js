1a69b9e4af394ff2197213681a2b7ffd6307364d
Seong ho Hong <grohong76@gmail.com>
2017-12-19 16:52:02 +0900

    Update 2017-12-19T07:51:56.987Z

--- js/app.js
+++ js/app.js
@@ -1,1 +1,1 @@
-console.log("my-note app.js"),$(function(){console.log("start page")}),$(".btn-newnote").click(function(){console.log("click new note")});
+console.log("my-note app.js"),$(function(){console.log("start page")}),$(".btn-newnote").click(function(){console.log("click new note")}),$(".btn-download").click(function(){console.log("click download")}),$(".btn-savenote").click(function(){console.log("click save note")}),$(".btn-fullscreen").click(function(){console.log("click fullscreen note")}),$("#about").click(function(){console.log("click about wee-note")});
