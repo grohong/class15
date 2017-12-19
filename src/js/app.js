e757bbfd90f309b4916d9954f0d968b57f4729a0
Seong ho Hong <grohong76@gmail.com>
2017-12-19 17:41:21 +0900

    set save note

--- src/js/app.js
+++ src/js/app.js
@@ -1,26 +1,45 @@
 console.log('my-note app.js');
 
 //DOM
+var storage = window.localStorage;
+var memo = document.getElementById('memo');
+
+var newNote = function () {
+  storage.clear();
+  location.reload();
+  memo.value = '';
+};
+
+var saveNote = function () {
+  console.log(memo.value);
+  storage.setItem('note', memo.value);
+  alert('메모가 저장되었습니다.');
+};
+
+var readExistNote = function () {
+  if (storage.length !== 0) {
+  memo.value = storage.getItem('note');
+  console.log('get note');
+  }
+};
+
 $(function () {
   console.log('start page');
-})
-
-$('.btn-newnote').click(function(){
-  console.log('click new note');
+  readExistNote();
 });
 
-$('.btn-savenote').click(function() {
-  console.log('click download');
-})
+$('.btn-newnote').click(newNote);
+
+$('.btn-savenote').click(saveNote);
 
 $('.btn-savenote-file').click(function() {
   console.log('click save note');
-})
+});
 
 $('.btn-fullscreen').click(function() {
   console.log('click fullscreen note');
-})
+});
 
 $('.btn-about').click(function() {
   console.log('click about wee-note');
-})
+});
